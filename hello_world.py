ccf1f1f25a99b8149574a1f4e764f77fbd891990
Luke O'Malley <omalley1@mit.edu>
2013-01-06 23:38:50 -0500

    added function with arguements and default

--- hello_world.py
+++ hello_world.py
@@ -4,3 +4,16 @@ print "Hello World!"
 # print hello world when function is called
 # def hello():
 # 	print "From function hello: Hello World!"
+
+# print hello world with arguments
+# def hello_with_args(word):
+# 	print "Hello " + word
+
+# hello_with_args("Luke!")
+
+# print hello world with arguments
+# def hello_with_args_default(word="World!"):
+# 	print "Hello " + word
+
+# hello_with_args_default()
+# hello_with_args_default("Luke!")
