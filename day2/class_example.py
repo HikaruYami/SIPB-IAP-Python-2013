eed3f1efda58bb7f1adb9a069252010cc821869b
fluffy <codetaku@mit.edu>
2013-01-10 15:18:23 -0500

    Fixed class_example.py

--- day2/class_example.py
+++ day2/class_example.py
@@ -1,10 +1,12 @@
 class example_class:
-    class_list = []
-    class_counter = 0
-    def counter():
-        class_counter += 1
-        return class_counter
-    def class_push():
-        class_list.append(class_counter)
-    def class_pop():
-        return class_list.pop()
+    def __init__(self):
+        self.class_counter = 0
+        self.class_list = []
+        print "Initialized!"
+    def counter(self):
+        self.class_counter += 1
+        return self.class_counter
+    def class_push(self):
+        self.class_list.append(self.class_counter)
+    def class_pop(self):
+        return self.class_list.pop()
